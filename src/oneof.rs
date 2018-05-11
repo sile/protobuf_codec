3c5b13fba8edd1f1317aa95545d62a44e8b30b0f
Takeru Ohta <phjgt308@gmail.com>
2018-05-11 10:32:06 +0900

    Add one-of encoders

--- src/oneof.rs
+++ src/oneof.rs
@@ -127,7 +127,7 @@ macro_rules! impl_field_decode {
             $($f: OneOfFieldDecode),*
         {
             type Item = $oneof<$($f::Item),*>;
-
+ 
             fn start_decoding(&mut self, tag: Tag, wire_type: WireType) -> Result<bool> {
                 match self.index {
                     $($i => track!(self.fields.$j.finish_decoding()).map(|_| ())?),*,
@@ -183,10 +183,79 @@ macro_rules! impl_field_decode {
     }
 }
 
+macro_rules! impl_field_encode {
+    ($oneof:ident,[$($f:ident),*],[$($i:expr),*],[$($j:tt),*]) => {
+        impl<$($f),*> Encode for OneOf<($($f),*,)>
+        where
+            $($f: OneOfFieldEncode),*
+        {
+            type Item = $oneof<$($f::Item),*>;
+
+            fn encode(&mut self, buf: &mut [u8], eos: Eos) -> Result<usize> {
+                match self.index {
+                    $($i => track!(self.fields.$j.encode(buf, eos))),*,
+                    _ => Ok(0),
+                }
+            }
+
+            fn start_encoding(&mut self, item: Self::Item) -> Result<()> {
+                track_assert!(self.is_idle(), ErrorKind::EncoderFull);
+                match item {
+                    $($oneof::$f(v) => {
+                        self.index = $i;
+                        track!(self.fields.$j.start_encoding(v))
+                    }),*
+                    $oneof::None => Ok(()),
+                }
+            }
+
+            fn is_idle(&self) -> bool {
+                match self.index {
+                    $($i => self.fields.$j.is_idle()),*,
+                    _ => true,
+                }
+            }
+
+            fn requiring_bytes(&self) -> ByteCount {
+                match self.index {
+                    $($i => self.fields.$j.requiring_bytes()),*,
+                    _ => ByteCount::Finite(0),
+                }
+            }
+        }
+        impl<$($f),*> FieldEncode for OneOf<($($f),*,)>
+        where
+            $($f: OneOfFieldEncode),*
+        {
+        }
+        impl<$($f),*> OneOfFieldEncode for OneOf<($($f),*,)>
+        where
+            $($f: OneOfFieldEncode),*
+        {
+        }
+        impl<$($f),*> ExactBytesEncode for OneOf<($($f),*,)>
+        where
+            $($f: OneOfFieldEncode + ExactBytesEncode),*
+        {
+            fn exact_requiring_bytes(&self) -> u64 {
+                match self.index {
+                    $($i => self.fields.$j.exact_requiring_bytes()),*,
+                    _ => 0,
+                }
+            }
+        }
+    };
+}
+
 #[derive(Debug, Default)]
 pub struct OneOf<F> {
     fields: F,
     index: usize,
+}
+impl<F> OneOf<F> {
+    pub fn new(fields: F) -> Self {
+        OneOf { fields, index: 0 }
+    }
 }
 impl_field_decode!(OneOf1, [A], [1], [0]);
 impl_field_decode!(OneOf2, [A, B], [1, 2], [0, 1]);
@@ -211,78 +280,27 @@ impl_field_decode!(
     [1, 2, 3, 4, 5, 6, 7, 8],
     [0, 1, 2, 3, 4, 5, 6, 7]
 );
-impl<F0, F1> Encode for OneOf<(F0, F1)>
-where
-    F0: OneOfFieldEncode,
-    F1: OneOfFieldEncode,
-{
-    type Item = OneOf2<F0::Item, F1::Item>;
 
-    fn encode(&mut self, buf: &mut [u8], eos: Eos) -> Result<usize> {
-        match self.index {
-            0 => Ok(0),
-            1 => track!(self.fields.0.encode(buf, eos)),
-            2 => track!(self.fields.1.encode(buf, eos)),
-            _ => unreachable!(),
-        }
-    }
-
-    fn start_encoding(&mut self, item: Self::Item) -> Result<()> {
-        track_assert!(self.is_idle(), ErrorKind::EncoderFull);
-        match item {
-            OneOf2::A(v) => {
-                self.index = 1;
-                track!(self.fields.0.start_encoding(v))
-            }
-            OneOf2::B(v) => {
-                self.index = 2;
-                track!(self.fields.1.start_encoding(v))
-            }
-            OneOf2::None => Ok(()),
-        }
-    }
-
-    fn is_idle(&self) -> bool {
-        match self.index {
-            0 => true,
-            1 => self.fields.0.is_idle(),
-            2 => self.fields.1.is_idle(),
-            _ => unreachable!(),
-        }
-    }
-
-    fn requiring_bytes(&self) -> ByteCount {
-        match self.index {
-            0 => ByteCount::Finite(0),
-            1 => self.fields.0.requiring_bytes(),
-            2 => self.fields.1.requiring_bytes(),
-            _ => unreachable!(),
-        }
-    }
-}
-impl<F0, F1> FieldEncode for OneOf<(F0, F1)>
-where
-    F0: OneOfFieldEncode,
-    F1: OneOfFieldEncode,
-{
-}
-impl<F0, F1> OneOfFieldEncode for OneOf<(F0, F1)>
-where
-    F0: OneOfFieldEncode,
-    F1: OneOfFieldEncode,
-{
-}
-impl<F0, F1> ExactBytesEncode for OneOf<(F0, F1)>
-where
-    F0: OneOfFieldEncode + ExactBytesEncode,
-    F1: OneOfFieldEncode + ExactBytesEncode,
-{
-    fn exact_requiring_bytes(&self) -> u64 {
-        match self.index {
-            0 => 0,
-            1 => self.fields.0.exact_requiring_bytes(),
-            2 => self.fields.1.exact_requiring_bytes(),
-            _ => unreachable!(),
-        }
-    }
-}
+impl_field_encode!(OneOf1, [A], [1], [0]);
+impl_field_encode!(OneOf2, [A, B], [1, 2], [0, 1]);
+impl_field_encode!(OneOf3, [A, B, C], [1, 2, 3], [0, 1, 2]);
+impl_field_encode!(OneOf4, [A, B, C, D], [1, 2, 3, 4], [0, 1, 2, 3]);
+impl_field_encode!(OneOf5, [A, B, C, D, E], [1, 2, 3, 4, 5], [0, 1, 2, 3, 4]);
+impl_field_encode!(
+    OneOf6,
+    [A, B, C, D, E, F],
+    [1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5]
+);
+impl_field_encode!(
+    OneOf7,
+    [A, B, C, D, E, F, G],
+    [1, 2, 3, 4, 5, 6, 7],
+    [0, 1, 2, 3, 4, 5, 6]
+);
+impl_field_encode!(
+    OneOf8,
+    [A, B, C, D, E, F, G, H],
+    [1, 2, 3, 4, 5, 6, 7, 8],
+    [0, 1, 2, 3, 4, 5, 6, 7]
+);
